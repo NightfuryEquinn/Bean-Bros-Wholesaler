25c82c45b963e6f2bbaec0045081099c5d245b9c
littsen <littsen.ong95@gmail.com>
2022-02-21 22:57:48 +0800

    description all done

--- script.js
+++ script.js
@@ -78,11 +78,13 @@ function popup(num, imgurl) {
     var captionText = document.getElementById("caption");
     var description = document.getElementById("imgPopUp").alt;
 
+    var content = document.getElementsByClassName("content")[0];
     var name = document.getElementsByClassName("desc-name")[0];
     var country = document.getElementsByClassName("desc-country")[0];
     var roast = document.getElementsByClassName("desc-roast")[0];
     var price = document.getElementsByClassName("desc-price")[0];
 
+    var content1 = document.getElementsByClassName("content")[newnum + 1].innerHTML;
     var name1 = document.getElementsByClassName("desc-name")[newnum + 1].innerHTML;
     var country1 = document.getElementsByClassName("desc-country")[newnum + 1].innerHTML;
     var roast1 = document.getElementsByClassName("desc-roast")[newnum + 1].innerHTML;
@@ -92,6 +94,7 @@ function popup(num, imgurl) {
     modal.style.display = "block";
     modalImg.src = imgurl;
     captionText.innerHTML = description;
+    content.innerHTML = content1;
     name.innerHTML = name1;
     country.innerHTML = country1;
     roast.innerHTML = roast1;
