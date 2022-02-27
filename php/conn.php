03e007a359247cd8faf6f7953cd20df2641caa01
Equinn Nightfury <xianzyip8@gmail.com>
2022-02-27 19:52:05 +0800

    Done connect to database

---
+++ php/conn.php
@@ -0,0 +1,16 @@
+<?php
+    $con = mysqli_connect("localhost", "root", "", "bean_bros");
+
+    // Check Connection
+    if(mysqli_connect_errno()) {
+        echo 'Failed to connect to PHPmyAdmin / MySQL: ' . mysqli_connect_errno();
+        echo '<script>
+        alert ("Unable to connect!");
+        </script>';
+        
+    } else {
+        echo '<script>
+        alert ("Connected!");
+        </script>';
+    }
+?>
